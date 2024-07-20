5292fe14f811b7670eb411e096242cfc177fb8ec
Luis <solarluiso@gmail.com>
2024-07-20 15:11:44 +0100

    Add "about me" section as default

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -165,14 +165,14 @@ const About = () => {
     >
       <div className="container mx-auto xl:h-[564px]">
         <Tabs
-          defaultValue="experience"
+          defaultValue="about"
           className="flex flex-col xl:flex-row gap-[60px]"
         >
           <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="about">About me</TabsTrigger>
             <TabsTrigger value="experience">Experience</TabsTrigger>
             <TabsTrigger value="education">Education</TabsTrigger>
             <TabsTrigger value="skills">Skills</TabsTrigger>
-            <TabsTrigger value="about">About me</TabsTrigger>
           </TabsList>
 
           {/* content */}
