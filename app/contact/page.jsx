460e83b59bd6fc6e618f07c04e8f94d7106b1926
Luis <solarluiso@gmail.com>
2024-10-22 00:47:16 -0400

    rearrange components on contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -21,8 +21,8 @@ const info = [
   },
   {
     icon: <FaMapMarkerAlt />,
-    title: "Address",
-    description: "Washington, DC 20001",
+    title: "Location",
+    description: "Washington, DC",
   },
 ];
 
@@ -35,25 +35,24 @@ const Contact = () => {
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px]">
+        <div className="flex flex-col justify-center items-center gap-6 py-[30px] xl:mb-8">
+          <h3 className="text-4xl font-bold text-center">Let's connect!</h3>
+          <p className="text-white/60 max-w-[768px] mx-auto text-center">
+            Reach out to discuss opportunities, projects, or simply to start a
+            conversation. Fill out the form below to get in touch.
+          </p>
+        </div>
+        <div className="flex flex-col xl:flex-row items-center gap-[30px]">
           {/* form */}
-          <div className="xl:w-[54%] order-2 xl:order-none">
+          <div className="order-2 xl:order-none w-full max-w-xl">
             <form className="flex flex-col gap-6 p-10 bg-secondary rounded-xl">
-              <h3 className="text-4xl text-accent">Let's connect!</h3>
-              <p className="text-white/60">
-                Reach out to discuss opportunities, projects, or simply to start
-                a conversation. Fill out the form below to get in touch.
-              </p>
               {/* input */}
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname" />
-                <Input type="lastname" placeholder="Lastname" />
-              </div>
-              <Input type="email" placeholder="Email address" />
+              <Input type="name" placeholder="Your name" />
+              <Input type="email" placeholder="Your email" />
               {/* textarea */}
               <Textarea
                 className="h-[200px]"
-                placeholder="Type your message here."
+                placeholder="Type your message here!"
               />
               {/* btn */}
               <Button size="md" className="max-w-40">
@@ -62,7 +61,7 @@ const Contact = () => {
             </form>
           </div>
           {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+          <div className="flex-1 flex items-center justify-center my-8 xl:mb-0">
             <ul className="flex flex-col gap-10">
               {info.map((item, index) => {
                 return (
