1b8ec24adcd02da37c73fe36a95c3c8b4ee3ff94
Luis <solarluiso@gmail.com>
2024-10-22 00:46:40 -0400

    adjust font size

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -184,7 +184,7 @@ const About = () => {
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                   {about.description}
                 </p>
-                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 mx-auto xl:mx-0">
                   {about.info.map((item, index) => {
                     return (
                       <li
