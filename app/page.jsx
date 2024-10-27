c2db958e1ac066e7abe803d21819ba85b973bb2d
Luis <solarluiso@gmail.com>
2024-10-27 10:02:52 -0400

    animate hero text area

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 
 import { FiDownload } from "react-icons/fi";
 import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/variants";
 
 // components
 import { Button } from "@/components/ui/button";
@@ -18,31 +19,55 @@ const Home = () => {
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-16 xl:pb-28 xl:px-16">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-16 xl:pb-28 xl:px-28">
           {/* text */}
-          <div className="flex flex-col gap-6 order-2 xl:order-none w-auto max-w-[490px] xl:max-w-[490px] text-center xl:text-left">
+          <div className="flex flex-col gap-6 order-2 xl:order-none w-auto max-w-[490px] xl:max-w-[490px] text-center">
             {/* hello */}
-            <h1 className="h1 flex flex-col">
+            <motion.h1
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.2 }}
+              className="h1 flex flex-col"
+            >
               <span className="">Hello!</span>
               <span>
                 I'm <span className="text-accent">Luiso</span>
               </span>
-            </h1>
+            </motion.h1>
             {/* bio */}
-            <div className="flex flex-col gap-4">
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.2 }}
+              className="flex flex-col gap-2 xl:text-left"
+            >
               <h3 className="h3 text-white/60">Web Developer</h3>
               <p className="p max-w-[400px] text-xl">
                 I build thoughtful and effective web solutions while sipping
                 coffee on a glorious day.
               </p>
-            </div>
+            </motion.div>
             {/* take action */}
-            <h3 className="h3">
+            <motion.h3
+              variants={fadeIn("up", 0.8)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.2 }}
+              className="h3 xl:text-left"
+            >
               Let's create something <br />
               remarkable together!
-            </h3>
+            </motion.h3>
             {/* btn and socials */}
-            <div className="flex flex-col xl:flex-row items-center gap-8">
+            <motion.div
+              variants={fadeIn("up", 1.0)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.2 }}
+              className="flex flex-col xl:flex-row xl:mt-2 items-center gap-8"
+            >
               <a href="/assets/Luis-Solar-CV.pdf" download>
                 <Button
                   variant="outline"
@@ -59,7 +84,7 @@ const Home = () => {
                   iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
                 />
               </div>
-            </div>
+            </motion.div>
           </div>
           {/* photo */}
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
