1df390f216402e44cf2639bf885bc2958c8d3038
Luis <solarluiso@gmail.com>
2025-05-05 12:18:29 -0400

    make header sticky to top

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white">
+    <header className="sticky top-0 bg-primary z-10 py-8 xl:py-12 text-white">
       <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
         <Link href="/">
